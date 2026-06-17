0884f6317eea7b93eb13cf94040071f7f7ccdcbf
Julia Wollena <wxulia2020@gmail.com>
2026-06-17 11:07:21 -0300

    Exercicios de if e else

---
+++ l041q06.js
@@ -0,0 +1,27 @@
+//Desenvolver um programa que pergunte dois valores numéricos inteiros e apresente o valor da diferença entre o
+//maior valor e o menor valor lido.
+
+let num1, num2, diferenca;
+
+num1 = 8
+console.log (`Insira o primeiro número:${num1}`)
+
+num2 = 1548
+console.log (`Insira o segundo número: ${num2}`)
+
+if (num1 > num2) {
+    diferenca = num1 - num2
+    console.log (`A diferença de ${num1} e ${num2} é ${diferenca}`)
+}
+else{
+    diferenca = num2 - num1
+    console.log (`A diferença entre ${num2} e ${num1} é ${diferenca}`)
+}
+
+
+
+
+
+
+
+
---
+++ l041q09.js
@@ -0,0 +1,16 @@
+let num;
+
+num = 0;
+console.log ("Informe um número");
+
+if (num > 0) {
+    console.log (`${num} é o positivo`);
+}
+else if (num < 0){
+    console.log (`${num} é negativo`);
+} 
+else {
+    console.log (`${num} é nulo`);
+}
+
+// Exemplo com else if 
